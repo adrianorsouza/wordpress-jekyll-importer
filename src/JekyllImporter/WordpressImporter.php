f1b6e76e8b2211c3f68e1f2c6b75bf020cae7846
Adriano Rosa <adrianorosa@sapo.pt>
2014-10-07 20:18:51 -0300

    class refactor

--- src/JekyllImporter/WordpressImporter.php
+++ src/JekyllImporter/WordpressImporter.php
@@ -2,87 +2,167 @@
 
 namespace JekyllImporter;
 
+use JekyllImporter\FileHandler;
 use JekyllImporter\FileHandlerException;
 
 class WordpressImporter
 {
+    /**
+     * The path to the WXR filename
+     * @var string
+     */
+    public $WXR = 'WXR-data.xml';
 
-    public $_WXR = 'WXR-posts.xml';
+    /**
+     * The path to images imported from WP uploads
+     * @var string
+     */
+    public $path_image = '/images';
 
-    public $post_path = 'blog/_posts';
+    /**
+     * The path to blog files
+     * @var string
+     */
+    public $path_page = 'blog';
 
-    public $data_path = '_data';
+    /**
+     * The path to blog posts
+     * @var string
+     */
+    public $path_post = 'blog/_posts';
 
+    /**
+     * The path to data for yaml files
+     * @var string
+     */
+    public $path_data = '_data';
+
+    /**
+     * The path to the images imported from WP uploads
+     * @var string
+     */
     public $categ_path = 'blog/category';
 
-    public $template_post = "---\ntitle: %s\ncategory_label: %s\ncategory: %s\ndate: %s\nwp_id: %s\nwp_slug: %s\n---\n\n%s";
+    /**
+     * The path to the template for pages
+     * @var string
+     */
+    public $template_page = 'templates/template_page.md';
 
-    public $template_yaml = "- name: %s\n  slug: %s\n\n";
+    /**
+     * The path to the template for posts
+     * @var string
+     */
+    public $template_post = 'templates/template_post.md';
 
+    /**
+     * The path to the template for data category
+     * @var string
+     */
+    public $template_data = 'templates/template_data.yml';
+
+    /**
+     * The path to the template for category pages
+     * @var string
+     */
     public $template_category = 'templates/template_category.md';
 
-    // public $category_content;
-
+    /**
+     * The file extension it can be .markdown or .html
+     * @var string
+     */
     public $output_ext = '.md';
 
+    /**
+     * Stores the main XML Object
+     * @var object
+     */
     private $_data;
 
-    private $_result = array();
+    /**
+     * File Handler Object
+     * @var object
+     */
+    private $_file;
 
+    /**
+     * Object Contructor
+     * @var void
+     */
     public function __construct($WXR = NULL)
     {
-
-    	date_default_timezone_set('UTC');
-
         if ( file_exists($WXR) ) {
-            $this->$_WXR = $WXR;
+            $this->WXR = $WXR;
         }
 
-        if ( !file_exists($this->_WXR) ) {
+        if ( !file_exists($this->WXR) ) {
             exit("WXR Not Found \n");
         }
 
-        $xml_data = new \SimpleXMLElement($this->_WXR, LIBXML_COMPACT, true);
+        $xml_data = new \SimpleXMLElement($this->WXR, LIBXML_COMPACT, true);
         $this->_data = $xml_data->channel->item;
+
+        $this->_file = new FileHandler();
     }
 
     /**
-     * Impost Post
+     * Impost Wordpress Posts
      *
-     * @param string
      * @return void
      */
     public function importPost()
     {
         foreach ($this->_data as $item) {
 
-            $post_name = $this->setPostName($item->children('wp', true)->post_date, $item->children('wp', true)->post_name);
+            $post_type = (string)$item->children('wp', true)->post_type;
 
-            $post_content = sprintf($this->template_post,
-                    $item->title,
-                    $item->category,
-                    $item->category->attributes()->nicename,
-                    $item->children('wp', true)->post_date,
-                    $item->children('wp', true)->post_id,
-                    $item->children('wp', true)->post_name,
-                    $item->children('content', true)->encoded
-                );
+            if ( $post_type === 'post' ) {
 
-            if ( defined('DEBUG') && DEBUG ) {
-                if ( $item->category->attributes()->nicename == 'wordpress') {
-                    _pr( ( $this->importImages($post_content) ));
-                    // _pr( htmlentities( $this->normalizeHTML($post_content) ));
-                }
+                $post_basename = date('Y-m-d', strtotime($item->children('wp', true)->post_date)) . '-' . $item->children('wp', true)->post_name;
 
-                $this->importImages($post_content);
+                $filename = $this->formatFileName('post', $post_basename);
 
-            } else {
-                $post_content = $this->normalizeHTML($post_content);
+                $template_post = file_get_contents($this->template_post);
+
+                $post_content = sprintf($template_post,
+                        $item->title,
+                        $item->category,
+                        $item->category->attributes()->nicename,
+                        $item->children('wp', true)->post_date,
+                        $item->children('wp', true)->post_id,
+                        $item->children('wp', true)->post_name,
+                        $item->children('content', true)->encoded
+                    );
+
+            } elseif ($post_type === 'page') {
+
+                $post_name = $item->children('wp', true)->post_name;
+                $filename = $this->formatFileName('page',$post_name);
+
+                $template_page = file_get_contents($this->template_page);
+
+                $post_content = sprintf($template_page,
+                        $item->title,
+                        $post_name,
+                        $item->children('excerpt', true)->encoded,
+                        $item->children('content', true)->encoded
+                    );
             }
 
-            $this->writeFile($post_name, $post_content);
-        }
+            // Write post files
+            try {
 
+                if ( defined('DEBUG') && DEBUG ) {
+                    // for debug on browser
+                }
+
+                $this->_file->write( $filename, $this->normalizeHTML($post_content) );
+
+            } catch (FileHandlerException $e) {
+
+                $this->_file->setOutput('error', $e->getMessage());
+            }
+        }
     }
 
     /**
@@ -96,137 +176,147 @@ class WordpressImporter
      */
     public function importCategory()
     {
-        $yaml_format = array();
+        $yaml_format = [];
         $yaml_content = '';
 
-        if ( !file_exists($this->template_category) ) {
+        try {
 
-            $this->setVerbose('error', sprintf("Template for category not found --> %s", $this->template_category));
+            if ( !file_exists($this->template_category) ) {
+                throw new FileHandlerException(
+                    sprintf("Template for category not found --> %s", $this->template_category)
+                );
 
-        } else {
+            } else {
 
-            $template_category = file_get_contents($this->template_category);
+                $template_category = file_get_contents($this->template_category);
+            }
+
+            foreach ($this->_data as $item) {
+                $post_type = (string)$item->children('wp', true)->post_type;
+
+                if ( $post_type !== 'post' ) {
+                    continue;
+                }
+
+                $yaml_format[(string)$item->category] = (string)$item->category->attributes()->nicename;
+            }
+
+            asort($yaml_format);
+
+            $template_data = file_get_contents($this->template_data) . PHP_EOL;
+
+            foreach ($yaml_format as $label => $slug) {
+
+                $yaml_content .= sprintf($template_data, $label, $slug);
+                $filename = $this->formatFileName('category', $slug);
+                $this->_file->write( $filename, vsprintf($template_category, [$label, $slug]) );
+            }
+
+            $this->_file->write($this->path_data . '/categories.yml', trim($yaml_content,"\n\n"));
+
+        } catch (FileHandlerException $e) {
+
+            $this->_file->setOutput('error', $e->getMessage());
         }
-
-        foreach ($this->_data as $item) {
-            $yaml_format[(string)$item->category] = (string)$item->category->attributes()->nicename;
-        }
-
-        asort($yaml_format);
-
-        // _vd($yaml_format);
-
-        foreach ($yaml_format as $label => $slug) {
-
-            $yaml_content .= sprintf($this->template_yaml, $label, $slug);
-
-            $this->writeFile($this->categ_path . '/' . $slug . $this->output_ext, vsprintf($template_category, array($label, $slug)));
-        }
-
-        $this->writeFile($this->data_path . '/categories.yml', trim($yaml_content,"\n\n"));
     }
 
     /**
-     * Write Files
+     * Set absolute path
      *
-     * @date datetimestamp
-     * @author Adriano Rosa (http://adrianorosa.com)
-     *
-     * @param string
-     * @return void
+     * @param string $type
+     * @param string $basename
+     * @return string
      */
-    public function writeFile($filename, $content, $type = null)
+    public function formatFileName($type = 'post', $basename)
     {
-        if ( !file_exists($filename) ) {
+        switch ($type) {
+            case 'category':
+                $path = $this->categ_path;
+                break;
 
-            $message = "Created and Imported data to file --> %s";
+            case 'page':
+                $path = $this->path_page;
+                break;
 
-        } else {
-
-            $message = "Updated data to file --> %s";
+            case 'post':
+                $path = $this->path_post;
+                break;
         }
 
-        if ( ! $handle = @fopen($filename, 'w')) {
-            $this->setVerbose('error', sprintf("Unable to read a file --> %s", $filename));
-            return;
-        }
+        $basename = preg_replace('/\s/', '-', trim($basename));
 
-        flock($handle, LOCK_EX);
-
-        if ( fwrite($handle, $content) === FALSE ) {
-            $this->setVerbose('error', sprintf("Unable to create a file --> %s", $filename));
-            return;
-        }
-
-        $this->setVerbose('success', sprintf($message, $filename));
-
-        flock($handle, LOCK_UN);
-        fclose($handle);
-
-        return $this;
+        return strtolower($path . DIRECTORY_SEPARATOR . $basename . $this->output_ext);
     }
 
-    public function setPostName($date_string, $string_name, $format_slug = false)
-    {
-        if ( is_numeric($date_string) ) {
-            $date_string = $date_string;
-        } else {
-            $date_string = strtotime($date_string);
-        }
-
-        if ( $format_slug === TRUE ) {
-            $string_name = preg_replace('/\s/', '-', trim($string_name));
-        }
-
-        return strtolower($this->post_path . '/' . date('Y-m-d', $date_string) . '-' . $string_name . $this->output_ext);
-    }
-
-    public function normalizeHTML($string)
+    /**
+     * Normalize post content make it compatible with markdown
+     *
+     * @param string $content The content to be normalized
+     * @return string
+     */
+    public function normalizeHTML($content)
     {
         $normalize_text = array(
             '/<pre.*?>\s{1,}/is'           => '<pre><code>',
             '/\s{1,}<\/pre>/is'            => "</code></pre>", // Normalize code snippet
             '/\n{0,}<ol>|\s{1,}<ol>/is'    => "\n", // Ordered List
-            '/\s{1,}<\/ol>|\s{0,}<li>/is'  => '',
-            '/<\/li>/is'                   => "\n",
+            '/\s{1,}<\/ol>|\s{0,}<li>/is'  => '',   // remove extra spaces
+            '/<\/li>/is'                   => "\n", // Normalize lists
             '/<strong>/is'                 => "**", // Strong replacement
             '/\s+<\/strong>|<\/strong>/is' => "**", // Close strong
-            '/\[caption.*?\]?<a rel.*?>|<\/a>\[\/caption\]/is' => '',
-            '/http:\/\/blog\.adrianorosa\.com\/wp-content\/uploads/i' => '/images',
+            '/\[caption.*?\]?<a rel.*?>|<\/a>\[\/caption\]/is' => ''
         );
 
-        $string = preg_replace(array_keys($normalize_text), array_values($normalize_text), $string);
-
-        return $string;
+        $content = preg_replace(array_keys($normalize_text), array_values($normalize_text), $content);
+        $content = $this->normalizeImages($content);
+        return $content;
     }
 
-    public function importImages($string)
+    /**
+     * Normalize images and link tags by removing unecessary attributes
+     * converts from Wordpress absolute URL to relative image path
+     *
+     * @param string $content The content to be normalized
+     * @return string
+     */
+    public function normalizeImages($content)
     {
-        // $string = $this->_data
-        // $string = ((string)$this->_data->children('content', true)->encoded);
-       preg_match_all('/\<+img\s+.*?>/ i', $string, $matches);
+        $pattern = '/\<a.*?>+<img.*?<\/a>|<img.*?>/msU';
+        // $images_normalized = [];
 
-       // preg_match_all('/src="http:\/\/.*?.jpg/i', $string, $matches);
+        preg_match_all($pattern, $content, $wp_images);
 
-       $images = array();
+        if ( ! $wp_images[0] ) {
 
-       foreach ($matches[0] as $key => $value) {
-           $images[] = preg_replace('/http:\/\/blog\.adrianorosa\.com\/wp-content\/uploads/i', "/images", $value);
-       }
+            return $content;
 
-       // _vd($string);
+        } else {
 
-        // $string = $matches;// _pr($matches);
+            foreach ($wp_images[0] as $key => $value) {
+                $normalized = preg_replace('/(https?:\/\/(.*)\/wp-content\/uploads)/i', $this->path_image, $value);
+                $normalized = preg_replace('/(<img.*?)(class="[^"]*"\s)/i', '$1', $normalized);
+                $images_normalized[] = $normalized;
+            }
 
-       // return $matches;
-       // return $string;
-       return $images;
+            preg_match_all($pattern, $content, $wp_images_to_fix);
+
+            $content = str_replace($wp_images_to_fix[0], $images_normalized, $content);
+        }
+
+        return $content;
     }
 
-    public function run(array $argv)
+    /**
+     * Execute Importer Class
+     *
+     * @param array $argv Accept an array of arguments passed via CLI command
+     * @return void
+     */
+    public function run(array $argv = [])
     {
         $count = 0;
         $read_item = 'all';
+        unset($argv[0]);
 
         if ( isset($argv['type'])) {
             if ( $argv['type'] == 'images' ) {
@@ -236,26 +326,26 @@ class WordpressImporter
 
         if ( $this->is_cli() ) {
 
-            if(!defined("STDIN")) {
-                // define("STDIN", fopen('php://stdin','r'));
-                exit('STDIN NOT DEFINED!');
-            }
-
             if ( !isset($argv[1]) ) {
-                echo "type what you want to import? [category, post or all]? ";
-                $read_item = trim(fread(STDIN, 20));
+
+                if ( !function_exists('readline') ) {
+                    defined("STDIN") OR define("STDIN", fopen('php://stdin','r'));
+
+                    echo "what kind of data do you want to import? [category, post or all]? ";
+                    $read_item = trim(fread(STDIN, 20));
+
+                } else {
+
+                    $read_item = trim(readline("what kind of data do you want to import? [category, post or all]? "));
+                }
 
             } else {
 
                 $read_item = $argv[1];
             }
-
         }
 
         switch ($read_item) {
-            case 'images':
-                $this->importImages();
-                break;
             case 'post':
                 $this->importPost();
                 break;
@@ -270,58 +360,11 @@ class WordpressImporter
                 break;
         }
 
-        foreach ($this->_result as $result) {
-
-            if ( isset($result['success']) ) {
-
-                $count++;
-                echo $result['success'];
-
-            } elseif ( isset($result['error']) ) {
-
-                $count --;
-                echo $result['error'];
-            }
-
-        }
-        echo "------------------------------". PHP_EOL;
-        echo ($count < 0) ? abs($count) . ' Errors found' : $count . ' Itens Was Imported';
-        echo PHP_EOL;
-        echo "------------------------------". PHP_EOL;
-        exit();
+        echo $this->_file->getOutput(); //exit();
     }
 
-    public function is_cli()
+    private function is_cli()
     {
-
         return (PHP_SAPI === 'cli' OR defined('STDIN'));
-    }
-
-    public function setVerbose($type = 'error', $string = null)
-    {
-        if ( $type === 'success' ) {
-
-            $message = sprintf("✔ Success %s", $string);
-
-        } else {
-
-            $message = sprintf("✗ Error %s", $string);
-        }
-
-        if ( $this->is_cli() ) {
-
-            $pretty = ($type === 'success')
-                ? "\033[0;32m"
-                : "\033[0;31m";
-
-            $message = $pretty . $message . "\033[0m" . PHP_EOL;
-
-        } else {
-
-            $message .= "<br>" . PHP_EOL;
-        }
-
-        $this->_result[][$type] = $message;
-        return $this;
     }
 }
